e15495651c38c71cda8ad232c1db9694fe0590c2
cloudafrica-gh <appdev@cloud-africa.io>
2020-02-03 10:15:01 +0000

    init site sil

---
+++ src/app/app.module.ts
@@ -0,0 +1,66 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutAreaComponent } from './components/about-area/about-area.component';
+import { PartnerAreaComponent } from './components/partner-area/partner-area.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { PricingPlanComponent } from './components/pricing-plan/pricing-plan.component';
+import { FreeTrialAreaComponent } from './components/free-trial-area/free-trial-area.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { TeamComponent } from './components/pages/team/team.component';
+import { PricingComponent } from './components/pages/pricing/pricing.component';
+import { ServicesComponent } from './components/pages/services/services.component';
+import { ServiceDetailsComponent } from './components/pages/service-details/service-details.component';
+import { BlogComponent } from './components/pages/blog/blog.component';
+import { BlogDetailsComponent } from './components/pages/blog-details/blog-details.component';
+import { PartnerComponent } from './components/pages/partner/partner.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { FaqComponent } from './components/pages/faq/faq.component';
+import { ComingSoonComponent } from './components/pages/coming-soon/coming-soon.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { MachineLearningComponent } from './components/pages/machine-learning/machine-learning.component';
+import { MachineLearningTwoComponent } from './components/pages/machine-learning-two/machine-learning-two.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { ProjectDetailsComponent } from './components/pages/project-details/project-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutAreaComponent,
+    PartnerAreaComponent,
+    FeedbackComponent,
+    PricingPlanComponent,
+    FreeTrialAreaComponent,
+    AboutComponent,
+    TeamComponent,
+    PricingComponent,
+    ServicesComponent,
+    ServiceDetailsComponent,
+    BlogComponent,
+    BlogDetailsComponent,
+    PartnerComponent,
+    NotFoundComponent,
+    FaqComponent,
+    ComingSoonComponent,
+    ContactComponent,
+    MachineLearningComponent,
+    MachineLearningTwoComponent,
+    ProjectsComponent,
+    ProjectDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/pages/machine-learning-two/machine-learning-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-learning-two',
+  templateUrl: './machine-learning-two.component.html',
+  styleUrls: ['./machine-learning-two.component.scss']
+})
+export class MachineLearningTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
